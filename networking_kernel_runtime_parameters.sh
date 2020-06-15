0c52d8fc5d9d16c294599af3f8f4d34bccc48d5b
Gavin Purcell <mkorangestripe@gmail.com>
2020-06-15 08:17:35 -0500

    Create networking_kernel_runtime_parameters.sh

---
+++ networking_kernel_runtime_parameters.sh
@@ -0,0 +1,35 @@
+# Enable packet forwarding:
+echo 1 > /proc/sys/net/ipv4/ip_forward
+/etc/sysctl.conf  # net.ipv4.ip_forward = 1
+
+# Drop icmp echo requests (pings).  Results in “Request timed out”
+echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_all
+/etc/sysctl.conf  # icmp_echo_ignore_all = 1
+
+# Return icmp echo broadcast requests:
+echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+/etc/sysctl.conf  # icmp_echo_ignore_all = 0
+
+# Using sysctl:
+sysctl -w net.ipv4.ip_forward=1  # sets ip_forward to 1
+sysctl net.ipv4.ip_forward  # shows current ip_forward value
+sysctl -p  # reread sysctl.conf - use after file is edited manually
+
+# Enable Connection Tracking  # either of the following:
+echo 1 > /proc/sys/net/netfilter/nf_conntrack_acct
+sysctl -w net.netfilter.nf_conntrack_acct=1
+
+# Adjust size of tracking table (nf_conntrack_max should be nf_conntrack_buckets x 4)
+echo 131072 > /proc/sys/net/netfilter/nf_conntrack_max
+echo 32768 > /sys/module/nf_conntrack/parameters/hashsize
+
+# Set values persistently in /etc/sysctl.conf:
+net.netfilter.nf_conntrack_acct=1
+net.netfilter.nf_conntrack_max=131072
+
+# Set the hashsize persistently in /etc/rc.d/rc.local:
+echo 32768 > /sys/module/nf_conntrack/parameters/hashsize
+
+# Show the current used connections (either of the following):
+cat /proc/sys/net/netfilter/nf_conntrack_count
+sysctl net.netfilter.nf_conntrack_count
