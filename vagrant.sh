11dfc24ba7c3fc803a032314ceccd2716f7f7693
Gavin Purcell <mkorangestripe@gmail.com>
2020-07-30 13:02:12 -0500

    Update vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -1,15 +1,23 @@
-VBoxManage list vms # list VirtualBox vm's
-VBoxManage list vms -l # list VirtualBox vm's with more details
-VBoxManage list hostonlyifs # list host-only network interfaces
+VBoxManage list vms  # list VirtualBox vm's
+VBoxManage list vms -l  # list VirtualBox vm's with more details
+VBoxManage list hostonlyifs  # list host-only network interfaces
 
-vagrant init bento/ubuntu-18.04. # create the Vagrant file with bento/ubuntu-18.04
-vagrant box add # to add additional boxes to the Vagrantfile
-vagrant global-status # get status of all VM's
-vagrant status
-vagrant up # download the box file if not already and start the VM
-vagrant suspend # suspends the machine
-vagrant halt # stops the vagrant machine
-vagrant destroy # shutdown VM, remove VirtualBox files, reclaim RAM
-vagrant box remove # also remove the box file - downloaded image
+# Login to Vagrant Cloud
+vagrant login
+vagrant cloud auth login
+
+vagrant global-status  # show status of all VMs
+vagrant status 2a44541  # show status of given VM
+
+vagrant box list  # list VMs
+vagrant box add  # to add additional boxes to the Vagrantfile
+vagrant box remove  #  remove the box file (downloaded image)
+
+vagrant init bento/ubuntu-18.04.  # create the Vagrant file with bento/ubuntu-18.04
+vagrant up  # download the box file if not already and start the VM
+vagrant suspend  # suspends the machine
+vagrant halt  # stops the vagrant machine
+vagrant destroy  # shutdown VM, remove VirtualBox files, reclaim RAM
+
 vagrant ssh
-vagrant ssh-config # show vagrant ssh info
+vagrant ssh-config  # show vagrant ssh info
