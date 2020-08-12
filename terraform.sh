25754722351f63579e4544568cdb81bfa9918d29
Gavin Purcell <mkorangestripe@gmail.com>
2020-08-12 08:29:29 -0500

    Create terraform.sh

---
+++ terraform.sh
@@ -0,0 +1,8 @@
+terrform fmt  # autoformat terraform scripts
+terraform validate  # check and report errors in config
+
+terraform init  # initialize a working directory containing Terraform configuration files
+terraform plan  # create an execution plan
+terraform apply  # apply changes
+terraform destroy  # terminate resources defined in Terraform configuration
+terraform show  # show state
