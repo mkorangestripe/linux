988641d9681ba084861a8fc857af1b1888781dc4
Gavin Purcell <mkorangestripe@gmail.com>
2025-04-07 11:52:22 -0500

    Revert "Update crontab scripts"
    This reverts commit f7bc17b738178dbaedca5aaedf3b10aa88ea7d1a.

--- package-updates/crontabs/apt-upgrade.sh
+++ package-updates/crontabs/apt-upgrade.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 # Cron driven package updater for systems with apt
 # Intended to run hourly within a window, 6am - 12pm
-# If out file is 6hrs (21600 seconds) or older, upgrade packages
-# If out file does not exist, create it, backdate it 6hrs, and upgrade packages
+# If out file is 17hrs (61200 seconds) or older, upgrade packages
+# If out file does not exist, create it, backdate it 17hrs, and upgrade packages
 # mtime of out file is updated initially to prevent overlapping hourly runs
 
 export TERM=xterm-256color
@@ -10,7 +10,7 @@ CRON_APT_OUT=/var/log/cron.apt.out
 
 test -f $CRON_APT_OUT ||
 touch -t $(date --date="17 hours ago" "+%Y%m%d%H%M") $CRON_APT_OUT &&
-test $(( $(date +"%s") - $(stat -c '%Y' $CRON_APT_OUT) )) -ge 21600 &&
+test $(( $(date +"%s") - $(stat -c '%Y' $CRON_APT_OUT) )) -ge 61200 &&
 { touch $CRON_APT_OUT
   /usr/bin/date
   /usr/bin/apt update &&
--- package-updates/crontabs/brew-upgrade.sh
+++ package-updates/crontabs/brew-upgrade.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Cron driven package updater for macOS with brew
 # Intended to run hourly within a window, 6am - 12pm
-# If out file is 6hrs (21600 seconds) or older, upgrade packages
-# If out file does not exist, create it, backdate it 6hrs, and upgrade packages
+# If out file is 17hrs (61200 seconds) or older, upgrade packages
+# If out file does not exist, create it, backdate it 17hrs, and upgrade packages
 # mtime of out file is updated initially to prevent overlapping hourly runs
 
 CRON_DIR=/Users/user1/cron
@@ -11,7 +11,7 @@ CRON_BREW_OUT=$CRON_DIR/cron.brew.out
 test -d $CRON_DIR || mkdir $CRON_DIR
 test -f $CRON_BREW_OUT ||
 touch -t $(date -v -17H "+%Y%m%d%H%M") $CRON_BREW_OUT &&
-test $(( $(date +"%s") - $(stat -f "%m" $CRON_BREW_OUT) )) -ge 21600 &&
+test $(( $(date +"%s") - $(stat -f "%m" $CRON_BREW_OUT) )) -ge 61200 &&
 { touch $CRON_BREW_OUT
   /bin/date; echo
   /usr/local/bin/brew upgrade; } > $CRON_BREW_OUT 2>&1
