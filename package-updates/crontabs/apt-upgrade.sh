ba571d14a69df2b5f338d460ee01ff3e93f64d8c
Gavin Purcell <mkorangestripe@gmail.com>
2025-04-12 18:39:11 -0500

    Update comments in crontab update scripts

--- package-updates/crontabs/apt-upgrade.sh
+++ package-updates/crontabs/apt-upgrade.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 # Cron driven package updater for systems with apt
 # Intended to run hourly within a window, 6am - 12pm
-# If out file is 6hrs (21600 seconds) or older, upgrade packages
-# If out file does not exist, create it, backdate it 6hrs, and upgrade packages
-# mtime of out file is updated initially to prevent overlapping hourly runs
+# If the out file is 6hrs (21600 seconds) or older, upgrade packages
+# If the out file does not exist, create it, backdate it 6hrs, and upgrade packages
+# mtime of the out file is updated initially to prevent potential overlapping hourly runs
 
 export TERM=xterm-256color
 CRON_APT_OUT=/var/log/cron.apt.out
